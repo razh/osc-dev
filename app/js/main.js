92b65306960142124ca97a6816bd4ce8596714d3
Raymond Zhou <somecrazyidiot@gmail.com>
2014-10-14 23:43:07 -0400

    Add basic TouchOSC data structures.

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,41 @@
   var socket = new WebSocket( 'ws://' + host + ':' + port );
 
   socket.addEventListener( 'message', function( event ) {
-    console.log( event );
+    var data = JSON.parse( event.data );
+    console.log( data );
   });
+
+  var types = [
+    'push',
+    'toggle',
+    'xy',
+    'fader',
+    'rotary',
+    'encoder',
+    'multitoggle',
+    'multixy',
+    'multipush',
+    'multifader'
+  ];
+
+
+  // Boolean.
+  function Push( value ) { this.value = value || false; }
+  function Toggle( value ) { this.value = value || false; }
+
+  // Numeric.
+  function Fader( value ) { this.value = value || 0; }
+  function Rotary( value ) { this.value = value || 0; }
+
+  // Coordinates.
+  function XY( x, y ) {
+    this.x = x || 0;
+    this.y = y || 0;
+  }
+
+  // Collections.
+  function MultiToggle( values ) { this.values = values || []; }
+  function MultiXY( values ) { this.values = values || []; }
+  function MultiPush( values ) { this.values = values || []; }
 
 }) ();
