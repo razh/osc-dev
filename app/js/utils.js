73b04734a72b2156284efd1c50371e3da6adebbc
Raymond Zhou <somecrazyidiot@gmail.com>
2014-11-09 23:07:33 -0500

    Add setter with lazy multi-dimensional array indexing.

--- app/js/utils.js
+++ app/js/utils.js
@@ -12,3 +12,28 @@ _.find = function( array, callback ) {
     }
   }
 };
+
+_.set = function( array, indices, value ) {
+  if ( !Array.isArray( array ) &&
+       !Array.isArray( indices ) &&
+       !indices.length ) {
+    return;
+  }
+
+  var index;
+  for ( var i = 0, il = indices.length; i < il; i++ ) {
+    index = indices[i];
+
+    // Set if last index.
+    if ( i === il - 1 ) {
+      array[ index ] = value;
+      return value;
+    }
+
+    if ( array[ index ] === undefined ) {
+      array[ index ] = [];
+    }
+
+    array = array[ index ];
+  }
+};
